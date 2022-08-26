91197adba346999c8f2b837b29543128101d866e
Mikel Giffin <giffinmike@hotmail.com>
2022-08-26 11:05:31 -0700

    Lulu Mountain Pass collision map done

--- client/src/App.js
+++ client/src/App.js
@@ -37,6 +37,7 @@ import IndoorHouse8 from './components/maps/indoorHouse8.js';
 import IndoorHouse9 from './components/maps/indoorHouse9.js';
 import IndoorHouse10 from './components/maps/indoorHouse10.js';
 import MountainTown from './components/maps/mountainTown.js';
+import LuluMountainPass from './components/maps/luluMountainPass.js';
 
 import DemoMap from './battleMaps/demoMap.jsx';
 import DemonObjects from './demonObjects.js'
@@ -98,6 +99,7 @@ const [demonTeam, setDemonTeam]= useState([DemonObjects.Player, DemonObjects.Dra
     indoorHouse9: <IndoorHouse9 active={tracker} adder={addItem} />,
     indoorHouse10: <IndoorHouse10 active={tracker} adder={addItem} />,
     mountainTown: <MountainTown active={tracker} adder={addItem} />,
+    luluMountainPass: <LuluMountainPass active={tracker} adder={addItem} />,
     demoMap: <DemoMap demonList={demonTeam} />,
   };
 
@@ -105,7 +107,7 @@ const [demonTeam, setDemonTeam]= useState([DemonObjects.Player, DemonObjects.Dra
 // console.log('POOP', mapsObj)
 
 
-const [current, setCurrent] = useState('mountainTown');
+const [current, setCurrent] = useState('luluMountainPass');
 
   function tracker(x) {
     setCurrent(x);
