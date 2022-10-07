b774ddc863aa31e921d0d545598dc0529d2670c2
Mikel Giffin <giffinmike@hotmail.com>
2022-10-07 14:01:25 -0700

    proxy try 1

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,10 +4,9 @@ module.exports = function (app) {
   app.use(
     '/state',
     createProxyMiddleware({
-      target: 'https://sacredcurese.com/',
+      target: 'https://sacredcurse.com/',
       secure: false,
       changeOrigin: true,
     })
   );
 };
-npm 
