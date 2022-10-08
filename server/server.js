b4cbca3e1ee336910331c775be61f8e00ab9f328
Mikel Giffin <giffinmike@hotmail.com>
2022-10-07 17:09:15 -0700

    adfsd

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.get('/state', cors(), async (req, res) => {
   const states = await State.find();
   res.setHeader(
     'Access-Control-Allow-Origin',
-    'https://www.pickel.co'
+   
   ); //sets the allow use to all requests html header
   res.json(states);
   // res.status(200);
